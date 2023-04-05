df5bf88963303e617f939e5c5ee632d410c9b11f
Denis <denisalalykin2001@gmail.com>
2023-04-05 20:31:15 +0700

    changeDB

--- app/src/main/java/com/denisal/studentsmarks/dbfunctions/DBSettings.kt
+++ app/src/main/java/com/denisal/studentsmarks/dbfunctions/DBSettings.kt
@@ -1,8 +1,4 @@
 package com.denisal.studentsmarks
-//const val user = "fan651sh_denisdb" //Имя пользователя БД
-//const val pass = "sgugitAdmin!" //Пароль к БД
-//const val url = "jdbc:mysql://fan651sh.beget.tech:3306/fan651sh_denisdb"
-
-const val user = "fan651sh_testing" //Имя пользователя БД
-const val pass = "Testing!" //Пароль к БД
-const val url = "jdbc:mysql://fan651sh.beget.tech:3306/fan651sh_testing"
+const val user = "fan651sh_denisdb" //Имя пользователя БД
+const val pass = "sgugitAdmin!" //Пароль к БД
+const val url = "jdbc:mysql://fan651sh.beget.tech:3306/fan651sh_denisdb"
